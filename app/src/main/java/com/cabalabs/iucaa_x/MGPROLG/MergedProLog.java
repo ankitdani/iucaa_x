7a4e79ca8f52ea0e4070cd5f54d782e4590d0e66
chinmay <chinmay2997@gmail.com>
2019-02-13 18:55:45 +0530

    Import files updated

--- app/src/main/java/com/cabalabs/iucaa_x/MGPROLG/MergedProLog.java
+++ app/src/main/java/com/cabalabs/iucaa_x/MGPROLG/MergedProLog.java
@@ -13,6 +13,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.cabalabs.iucaa_x.R;
+
 public class MergedProLog extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
